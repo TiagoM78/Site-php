de2028d2d59a9bff1848139c8f5e1d27045bb0de
Ahairshan78 <tiagomonteiro78570@outlook.fr>
2020-10-02 13:34:43 +0200

    ajout liste a défiler et case a cocher sécurité dans la balise prénom

--- index.php
+++ index.php
@@ -22,5 +22,15 @@
 	</p>
 	</form>
 
+	<select name="choix">
+    	<option value="choix1">Choix 1</option>
+    	<option value="choix2">Choix 2</option>
+    	<option value="choix3">Choix 3</option>
+    	<option value="choix4">Choix 4</option>
+	</select>
+
+	<input type="checkbox" name="case" id="case" /> <label for="case">Célibataire</label>
+	<input type="checkbox" name="case" id="case" /> <label for="case">Marier</label>
+
 	</body>
 </html>
